bf8a9d9fbbc9580db44989cd133ca128be656168
KanakoX <1308952861@qq.com>
2025-09-13 12:08:26 +0800

    first commit

---
+++ src/main/java/com/kanako/chatbaseonnetty/ChatBaseOnNettyApplication.java
@@ -0,0 +1,13 @@
+package com.kanako.chatbaseonnetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBaseOnNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBaseOnNettyApplication.class, args);
+    }
+
+}
