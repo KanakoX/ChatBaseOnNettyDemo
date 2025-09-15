875a9239531d44ca4d8a534737d35a275023e7a4
KanakoX <1308952861@qq.com>
2025-09-15 15:26:22 +0800

    配置redis

---
+++ src/main/java/com/kanako/chatbaseonnetty/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.kanako.chatbaseonnetty.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object>  redisTemplate = new RedisTemplate<>();
+        // 配置连接工厂
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        // 设置key的序列化器 String字符串的序列化器
+        // 所有通过RedisTemplate操作redis时，key都是字符串
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        // 设置value的序列化器 JSON字符串的序列化器
+        // 所有通过RedisTemplate操作redis时，value都是json字符串
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+        return redisTemplate;
+    }
+}
---
+++ src/main/java/com/kanako/chatbaseonnetty/setup/TestRedis.java
@@ -0,0 +1,24 @@
+package com.kanako.chatbaseonnetty.setup;
+
+import com.kanako.chatbaseonnetty.user.pojo.po.UserPO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRedis implements ApplicationRunner {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        ValueOperations<String, Object> ops = redisTemplate.opsForValue();
+        UserPO userPO = new UserPO();
+        userPO.setUsername("kanako");
+        userPO.setPassword("123456");
+        ops.set("kanako", userPO);
+    }
+}
